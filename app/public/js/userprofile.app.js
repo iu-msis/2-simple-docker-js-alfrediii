eb9539c37c21ecde5591f9ef0d94f69731c73568
Alfred Johnson III <alfjohn@iu.edu>
2020-09-18 05:00:04 -0400

    date fix

--- app/public/js/userprofile.app.js
+++ app/public/js/userprofile.app.js
@@ -15,15 +15,15 @@ var app = new Vue({
   },
   methods:{
     fetchRand: function() {
-      fetch("https://randomuser.me/api")
-      .then(response => response.json())
-      .then(data =>
+      fetch("https://randomuser.me/api").then(response => response.json()).then(data =>
         {
         var randUser = data.results[0]
         console.log(randUser);
         this.randName = randUser.name.first + ' ' + randUser.name.last;
         this.randEmail = randUser.email.slice(0,-11) + 'iu.edu';
-        this.randDOB = randUser.dob.date.format(mm/dd/yyy);
+        this.randDOB = randUser.dob.date.substring(5,7)+'/'
+        +randUser.dob.date.substring(8,10)
+        +'/'+randUser.dob.date.substring(0,4);
         this.randAge = randUser.dob.age;
         this.randCountry = randUser.location.city +', '+ randUser.location.country;
         this.randImg = randUser.picture.large;
