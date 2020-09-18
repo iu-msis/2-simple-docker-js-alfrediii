a82d916f45f50a4aeef900b26fa1ca2c13e1bd5f
Alfred Johnson III <alfjohn@iu.edu>
2020-09-18 03:15:10 -0400

    Coding errors

---
+++ app/public/js/userprofile.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: '#randomProfile',
+  data: {
+    randName:'',
+    randEmail:'',
+    randDOB:'',
+    randAge:'',
+    randCountry:'',
+    randImg:'',
+    randImgThumb:''
+  },
+
+  created(){
+    this.fetchRand();
+  },
+  methods:{
+    fetchRand: function() {
+      fetch('https://randomuser.me/api/').then(response => response.json()).then(data =>
+        {
+        var randUser = data.results[0]
+        console.log(randUser);
+        this.randName = randUser.name.first + '' + randUser.name.last;
+        this.randEmail = randUser.email;
+        this.randDOB = randUser.dob.date.substring(5,10) + '-' + randUser.dob.date.substring(0,4);
+        this.randAge = randUser.dob.age;
+        this.randCountry = randUser.location.country;
+        this.randImg = randUser.picture.large;
+        this.randImgThumb = randUser.picture.thumbnail;
+      })
+    },
+  },
+})
